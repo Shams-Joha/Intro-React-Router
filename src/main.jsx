f15e9c9cc2493e47448173e6c161c03234c0d693
Shams-Joha <shamsjoha3@gmail.com>
2024-11-01 15:02:08 +0600

    implemented nested routes with outlet and used links

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello from react router!</div>
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
+
 ]);
 
 createRoot(document.getElementById('root')).render(
